65ad95bbdb6544571027a38c8a8a8b6d2b61064c
Lewis Kabui <lewis.maina@andela.com>
2016-10-14 21:44:20 +0300

    Create an array of heroes
    Create an array of Hero objects and expose it in the AppComponent
    through a heroes attribute

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,20 @@ export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'Rubberman' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -22,4 +36,5 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
 }
