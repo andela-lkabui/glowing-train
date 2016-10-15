6ee51dc88ac6cacc8ea4ce8131e6a28f41354475
Lewis Kabui <lewis.maina@andela.com>
2016-10-15 23:04:54 +0300

    Define getHeroesSlowly() to simulate a 2 second latency

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,7 +6,15 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
+  }
+
+  // simulates a delay of 2 seconds
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(resolve, 2000))
+      .then(() => this.getHeroes());
   }
 }
