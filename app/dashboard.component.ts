9d63400f83a9a5d2ca3df475632c14488db70083
Lewis Kabui <lewis.maina@andela.com>
2016-10-19 20:10:31 +0300

    Include relevant imports to user HeroService

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @Component({
   moduleId: module.id,
@@ -6,4 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit{
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(
+      heroes => this.heroes = heroes.slice(1, 5)
+    );
+  }
+
+  gotoDetail(hero: Hero): void {}
+}
