93963db57528dcd5ccd7612307cb5508febc6254
Lewis Kabui <lewis.maina@andela.com>
2016-10-20 21:33:38 +0300

    Implement gotoDetail method
    - Flesh out implementation of this method.
    - Create a link array containing the path and id of the selected Hero
    respectively
    - Pass this array as an argument to the navigate method of router, which
    then refers to the route definitions in app.module.ts and finds the
    parameterized url (detail/:id) and concludes the operation

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -13,7 +14,10 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit{
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes().then(
@@ -21,5 +25,8 @@ export class DashboardComponent implements OnInit{
     );
   }
 
-  gotoDetail(hero: Hero): void {}
+  gotoDetail(hero: Hero): void {
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
