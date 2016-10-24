5d31f48ea047ae352473fd0b47524e6ea90930e2
Lewis Kabui <lewis.maina@andela.com>
2016-10-24 18:10:54 +0300

    Import and include HttpModule
    Import HttpModule in the app.module.ts file and include it in the
    imports array so as to avail the complete set of http services to
    the entire app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }   from './app.component';
 import { HeroService } from './hero.service';
@@ -15,6 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
