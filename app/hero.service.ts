57f4d4e6af893d8ce608cc4e53e689f7f5f0a305
Lewis Kabui <lewis.maina@andela.com>
2016-10-25 18:21:57 +0300

    getHeroes from a mock server
    - Change getHeroes implementation to get heroes data from the
    InMemoryDbService implementation
    - http.get returns a barebones Observable. rxjs... is imported because it
    has capabilities to convert Observables into promises through toPromise
    - Promise has a then callback that has access to a Http Response object
    which has a json() method. This method returns an object which has a data
    property
    - In this case, the data property is the list of heroes that was required

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
-
 
 @Injectable()
 export class HeroService {
 
+  private heroesUrl = 'app/heroes';
+
+  constructor(private http: Http) {}
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   // simulates a delay of 2 seconds
